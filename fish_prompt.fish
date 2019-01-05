a85ff53674c9009e0e31a026bb420afe2076e074
Gabe Dunn <gabe.dunn@shaw.ca>
2019-01-05 04:20:40 -0700

    add actual theme

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,3 +1,45 @@
+# name: redfish
+
+function _prompt_whoami -d "Display user@host if on a SSH session"
+  if set -q SSH_TTY
+    printf '(%s@%s)' (whoami) (hostname)
+  end
+end
+
+function _git_branch_name -d "Return the git branch name"
+  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+end
+
+function _is_git_dirty -d "Return whether the git repo is dirty"
+  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
+end
+
+function _git_display -d "Display the git branch & status"
+  if [ (_git_branch_name) ]
+    set_color green
+    if [ (_is_git_dirty) ]
+      printf ':%s*' (_git_branch_name)
+    else
+      printf ':%s' (_git_branch_name)
+    end
+    set_color blue
+  end
+end
+
 function fish_prompt
-  # Customize fish prompt
+  set -l exit_code $status
+
+  set_color -o red
+  printf 'λ '
+
+  set_color cyan
+  printf '%s ' (_prompt_whoami)
+
+  set_color blue
+  printf '{ %s%s }\n' (prompt_pwd) (_git_display)
+
+  set_color red
+  printf '» '
+
+  set_color normal
 end
--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,3 +1,15 @@
 function fish_right_prompt
-  # Customize the right prompt
+  set -l exit_code $status
+
+  set_color blue
+  printf '[%s]' (pwd | sed "s|^$HOME|~|")
+  if test $exit_code -ne 0
+    set_color f00
+  else
+    set_color 0f0
+  end
+  printf '[%d]' $exit_code
+  set_color cyan
+  printf '[%s]' (date +%H:%M)
+  set_color normal
 end
