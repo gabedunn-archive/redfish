74fa03f953d65615139a93865e1e014bb1fdf451
Gabriel Dunn <gabe.dunn@shaw.ca>
2019-01-08 13:05:23 -0700

    add current command to title

--- fish_title.fish
+++ fish_title.fish
@@ -1,3 +1,11 @@
 function fish_title
-  printf '%s@%s: %s' (whoami) (hostname) (pwd | sed "s|^$HOME|~|")
+  # save current command to variable
+  set -l current_command $argv[1]
+
+  # if there is no current command, show 'user@host: pwd' (with ~), otherwise show current command
+  if test -z "$current_command"
+    printf '%s@%s: %s' (whoami) (hostname) (pwd | sed "s|^$HOME|~|")
+  else
+    printf '%s' $current_command
+  end
 end
